ac283bc33bfe27157c864bbd42d43f449dab844c
Mankur11 <mankur@inbox.ru>
2019-11-13 16:02:50 +0300

    Working on responsive adjustments, adding a sidedrawer toggle button and hamburger icon

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,14 +2,16 @@ import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
+import Backdrop from "../../UI/Backdrop/Backdrop";
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
+        <DrawerToggle clicked={props.drawerToggleClicked}/>
         <div className={classes.Logo}>
             <Logo/>
         </div>
-        <nav>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems/>
         </nav>
     </header>
